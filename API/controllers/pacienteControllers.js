0fde04c35db2232776dfdb7f8641c6a61ac8865d
ignaciocervino <ignaciocervino@gmail.com>
2021-07-04 23:55:19 -0300

    Creating controller

---
+++ API/controllers/pacienteControllers.js
@@ -0,0 +1,8 @@
+
+
+// Cuando se crea un nuevo cliente
+exports.nuevoCliente = (req,res,next)=>{
+    // TODO: Insertar en la base de datos
+
+    resizeBy.json({mensaje: 'El cliente se agrego correctamente'});
+}
