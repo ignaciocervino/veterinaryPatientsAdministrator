5ca55cd72bc76c2f103815a0d9a4d25e4d7c1f0f
ignaciocervino <ignaciocervino@gmail.com>
2021-07-05 01:30:15 -0300

    Using axios to consume API

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,29 @@
-import React from 'react';
+import React,{useEffect,useState} from 'react';
 import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+
+import clienteAxios from './config/axios';
 //Componentes
 import Pacientes from './components/Pacientes';
 import NuevaCita from './components/NuevaCita';
 import Cita from './components/Cita';
 function App() {
+
+  //State de la app
+  const [citas,guardarCitas] = useState([]);
+
+  useEffect(()=>{//Buen lugar para consumir una API externa
+    const consultarAPI = ()=>{
+      clienteAxios.get('/pacientes')
+        .then(respuesta=>{
+
+        })
+        .catch(error=>{
+          console.log(error);
+        })
+    }
+    consultarAPI();
+  },[]);//[] son las dependecias
+
   return (
     <Router>
       <Switch>
