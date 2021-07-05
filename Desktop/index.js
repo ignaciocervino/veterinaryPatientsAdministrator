698d5e8073086cda0ea871558af140c31ede8edf
ignaciocervino <ignaciocervino@gmail.com>
2021-07-05 13:33:41 -0300

    starting app

---
+++ Desktop/index.js
@@ -0,0 +1,24 @@
+const {app,BrowserWindow} = require('electron');
+
+let appWindow;
+//Ventana principal del proyecto
+function crearVentana(){
+    appWindow = new BrowserWindow({
+
+    });
+
+    //Cuando la aplicacion es cerrada
+    appWindow.on('closed',()=>{
+        appWindow = null;
+    });
+
+    //Cargar HTML
+    appWindow.loadFile('./index.html');
+
+    //Cuando la app este lista, mostrar la ventana
+    appWindow.once('ready-to-show',()=>{
+        appWindow.show();
+    });
+}
+
+app.on('ready',crearVentana);
