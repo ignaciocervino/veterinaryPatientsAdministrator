f451fba9fd1eeb80d92e21a4bb209b550db6dfe9
ignaciocervino <ignaciocervino@gmail.com>
2021-07-04 23:49:13 -0300

    Connecting to MongoDB

--- API/index.js
+++ API/index.js
@@ -1,7 +1,16 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
 //Crear el servidor
 const app = express();
+
+//Conectar a mongodb
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://localhost/veterinaria',{
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false
+});
 
 // puerto y arrancar el servidor
 app.listen(4000,()=> {
