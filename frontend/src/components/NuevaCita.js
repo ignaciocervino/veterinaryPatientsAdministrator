d6492aac13805772cc61dbd5352f17221f368334
ignaciocervino <ignaciocervino@gmail.com>
2021-07-05 01:23:57 -0300

    env variables

---
+++ frontend/src/components/NuevaCita.js
@@ -0,0 +1,6 @@
+import React from 'react';
+const NuevaCita = () => {
+    return ( <h1>Desde nueva cita</h1> );
+}
+ 
+export default NuevaCita;
---
+++ frontend/src/components/Pacientes.js
@@ -0,0 +1,6 @@
+import React from 'react';
+const Pacientes = () => {
+    return ( <h1>Pacientes</h1> );
+}
+ 
+export default Pacientes;
